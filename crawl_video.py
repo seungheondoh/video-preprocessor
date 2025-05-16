033c729262d7603840bb7f00e3d121c205f614ba
Minhee Lee <minigimbob@gmail.com>
2025-05-16 07:23:09 +0000

    No problem to run.

--- crawl_video.py
+++ crawl_video.py
@@ -15,10 +15,12 @@ from collections import defaultdict
 from gaudio_yt_videos_list.utils.id import get_channel_or_playlist_id
 
 # 기본 설정
-FAILED_LOG = "failed_ids_clip.txt"
-UPLOAD_FAILED_LOG = "upload_failed_ids.txt"
-COMPLETED_LOG = "complete_clip_ids.txt"
-DOWNLOAD_DIR = "/mnt/hdd8tb/downloads_clip"
+LOG_DIR = "logs"
+FAILED_LOG = f"{LOG_DIR}/failed_ids_clip.txt"
+UPLOAD_FAILED_LOG = f"{LOG_DIR}/upload_failed_ids.txt"
+COMPLETED_LOG = f"{LOG_DIR}/complete_clip_ids.txt"
+
+DOWNLOAD_DIR = "download_tmp_dir"
 CSV_PATH = "gaudio_yt_videos_list/channel_and_playlist/final_result.csv"
 TXT_DIR = 'gaudio_yt_videos_list/ytids'
 
@@ -115,26 +117,23 @@ def upload_clip_folder(clip_id):
 
     return success
 
-def download_and_upload(video_info):
-    video_id = video_info['video_id']
-    clip_id = video_info['clip_id']
-
+def download_and_upload(video_id):
     # # We already checked this in the main function
     # failed_ids = load_failed_ids()
     # completed_ids = load_completed_ids()
     # if clip_id in failed_ids or clip_id in completed_ids:
     #     return False
 
-    video_dir = os.path.join(DOWNLOAD_DIR, clip_id)
+    video_dir = os.path.join(DOWNLOAD_DIR, video_id)
 
     if os.path.exists(video_dir):
         shutil.rmtree(video_dir)
     os.makedirs(video_dir, exist_ok=True)
 
-    mp4_path_template = os.path.join(video_dir, f"{clip_id}.%(ext)s")
-    mp4_path = os.path.join(video_dir, f"{clip_id}.mp4")
-    mp3_path = os.path.join(video_dir, f"{clip_id}_audio.mp3")
-    json_path = os.path.join(video_dir, f"{clip_id}.info.json")
+    mp4_path_template = os.path.join(video_dir, f"{video_id}.%(ext)s")
+    mp4_path = os.path.join(video_dir, f"{video_id}.mp4")
+    mp3_path = os.path.join(video_dir, f"{video_id}_audio.mp3")
+    json_path = os.path.join(video_dir, f"{video_id}.info.json")
 
     # start_frame, end_frame = video_info['clip_start_end_idx']
     # fps = video_info['video_fps']
@@ -159,11 +158,11 @@ def download_and_upload(video_info):
         
         # ✅ 랜덤한 시간 지연 추가 (0.5초 ~ 1.5초)
         sleep_time = random.uniform(0.5, 1.5)
-        print(f"[WAIT] {clip_id} 다운로드 전 대기 중... ({sleep_time:.2f}초)")
+        print(f"[WAIT] {video_id} 다운로드 전 대기 중... ({sleep_time:.2f}초)")
         time.sleep(sleep_time)
 
-        # print(f">>> {clip_id} 다운로드 중... ({start_sec:.2f}s ~ {end_sec:.2f}s)")
-        print(f">>> {clip_id} 다운로드 중...")
+        # print(f">>> {video_id} 다운로드 중... ({start_sec:.2f}s ~ {end_sec:.2f}s)")
+        print(f">>> {video_id} 다운로드 중...")
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([f"https://www.youtube.com/watch?v={video_id}"])
 
@@ -171,24 +170,24 @@ def download_and_upload(video_info):
             extract_audio(mp4_path, mp3_path)
             
         if not (os.path.exists(mp4_path) and os.path.exists(mp3_path) and os.path.exists(json_path)):
-            log_failed(clip_id, "다운로드된 파일 없음")
+            log_failed(video_id, "다운로드된 파일 없음")
             if os.path.exists(video_dir):
                 shutil.rmtree(video_dir)
             return False
 
         # ✅ S3 업로드
-        if upload_clip_folder(clip_id):
+        if upload_clip_folder(video_id):
             shutil.rmtree(video_dir)
-            log_completed(clip_id)
-            print(f"업로드 성공: {clip_id}")
+            log_completed(video_id)
+            print(f"업로드 성공: {video_id}")
             return True
         else:
-            print(f"❌ S3 업로드 실패: {clip_id}")
+            print(f"❌ S3 업로드 실패: {video_id}")
             return False
 
     except Exception as e:
         error_msg = str(e).lower()
-        log_failed(clip_id, error_msg)
+        log_failed(video_id, error_msg)
 
         # 일반 실패 시 클린업
         if os.path.exists(video_dir):
@@ -235,16 +234,16 @@ def get_video_ids_per_category():
 
 def main():
     video_ids_df = get_video_ids_per_category()
-    clip_ids = video_ids_df['clip_id'].tolist()
+    video_ids = video_ids_df['video_id'].tolist()
     # remove duplicates
     # data
-    clip_ids = list(set(clip_ids))
+    video_ids = list(set(video_ids))
 
     failed_ids = load_failed_ids()
     completed_ids = load_completed_ids()
-    data = [clip_id for clip_id in clip_ids if clip_id not in failed_ids and clip_id not in completed_ids]
+    data = [video_id for video_id in video_ids if video_id not in failed_ids and video_id not in completed_ids]
 
-    print(f"🔍 처리할 clip_id 수: {len(data)}")
+    print(f"🔍 처리할 video 수: {len(data)}")
 
     with Pool(NUM_WORKERS) as pool:
         with tqdm(total=len(data), desc="다운로드 및 업로드 진행") as pbar:
